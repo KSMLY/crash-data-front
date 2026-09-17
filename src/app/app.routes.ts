dacace4944fb64cecf80f89bb631f6632c483b00
KSMLY <kmsmaily152@gmail.com>
2026-09-17 14:10:42 +0300

    CD-39: app shell with sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,14 @@
 import { Routes } from '@angular/router';
+import { Shell } from './shell/shell';
 import { CrashList } from './crash-list/crash-list';
 
-export const routes: Routes = [{ path: '', component: CrashList }];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Shell,
+    children: [
+      { path: '', component: CrashList },
+      { path: 'crashes', component: CrashList },
+    ],
+  },
+];
