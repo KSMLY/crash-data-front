c8fa30e2c47d10f77c370294bbfcf0098a1d3629
KSMLY <kmsmaily152@gmail.com>
2026-09-17 14:37:34 +0300

    CD-39: overview placeholder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
 import { Shell } from './shell/shell';
 import { Crashes } from './crashes/crashes';
+import { Overview } from './overview/overview';
 
 export const routes: Routes = [
   {
     path: '',
     component: Shell,
     children: [
-      { path: '', component: Crashes },
+      { path: '', component: Overview },
       { path: 'crashes', component: Crashes },
     ],
   },
